5bface1be29e01e9d55672077f1d473cfd530fdd
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-04-13 11:49:30 +0200

    #2 skapat och kopierat css-filerna bootstrap och font-awesome inkl länkat dess tillsammans med style.css i index.php. Sparat och lagt in bilden för temat.

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Labb 1</title>
-	<link href="/css/font-awesome.css" rel="stylesheet" type="text/css" />
-	<link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
-	<link href="/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="bootstrap.css" rel="stylesheet" type="text/css" />
+	
 	<script src="js/jquery.js"></script>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style.css'; ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/bootstrap.css'; ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/font-awesome.css'; ?>">
+
+
 </head>
 <body>
 
